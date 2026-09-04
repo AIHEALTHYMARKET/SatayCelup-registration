e4d4823696020296a453aa96d925797e1f1aaf45
AIHEALTHYMARKET <diabolo.weien@gmail.com>
2026-09-04 18:27:35 +0800

    Add Firebase configuration and initialization

---
+++ css/js/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { 
+    getAuth 
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCWaQbWLteRDMiGcyTdfJanDiuSiPUFdeU",
+    authDomain: "sataycelup-registration-af033.firebaseapp.com",
+    projectId: "sataycelup-registration-af033",
+    storageBucket: "sataycelup-registration-af033.firebasestorage.app",
+    messagingSenderId: "485590570714",
+    appId: "1:485590570714:web:d693b857a99e5bf821f7e3"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export { app };
